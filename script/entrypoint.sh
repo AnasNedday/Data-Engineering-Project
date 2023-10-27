5366549f5211ce86b1babdb6f68b7a11f069ab80
Anas Nedday <97068637+AnasNedday@users.noreply.github.com>
2023-10-27 11:03:37 +0100

    first commit

---
+++ script/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Check if requirements.txt file exists
+if [ -f /opt/airflow/requirements.txt ]; then
+  # Upgrade pip as the 'airflow' user
+  su -c "pip install --upgrade pip" - airflow
+
+  # Install Python packages from requirements.txt as the 'airflow' user
+  su -c "pip install -r /opt/airflow/requirements.txt" - airflow
+fi
+
+# Check if airflow.db file does not exist
+if [ ! -f /opt/airflow/airflow.db ]; then
+  # Initialize the Airflow database as the 'airflow' user
+  su -c "airflow db init" - airflow
+
+  # Create an admin user (adjust the parameters as needed)
+  su -c "airflow users create \
+    --username admin \
+    --firstname admin \
+    --lastname admin \
+    --role Admin \
+    --email admin@example.com \
+    --password admin" - airflow
+fi
+
+# Run the Airflow webserver as the 'airflow' user
+su -c "airflow webserver" - airflow
